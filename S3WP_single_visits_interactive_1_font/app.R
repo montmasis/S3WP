d5aa8158b706477ab9a08da331a92ad126748ff2
Arnold Kochari <arnold.kochari@scilifelab.se>
2024-04-12 08:31:31 +0200

    correct shiny app launch command

--- S3WP_single_visits_interactive_1_font/app.R
+++ S3WP_single_visits_interactive_1_font/app.R
@@ -25,5 +25,4 @@ server = function(input, output, session) {
 
 cat('If the shiny app is not automatically opened in the browser, you can manually
 copy the following link and paste it to the browser.');
-print(shinyApp(ui, server, options = app_options))
-
+shinyApp(ui = ui, server = server)
